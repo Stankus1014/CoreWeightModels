d9f22f52967d784538b5b4adebdeffe213984663
Will Stankus <will.stankus@gmail.com>
2025-10-20 20:09:23 -0500

    created CoreWeightModels swift package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CoreWeightModels",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -13,8 +16,6 @@ let package = Package(
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CoreWeightModels"
         ),
